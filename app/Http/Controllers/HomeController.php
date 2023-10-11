0491d7aa6718e97c2fc2d40e9cdf8a3b991e72c9
Dani Setiawan <danisetiawan609@gmail.com>
2023-10-11 15:47:23 +0700

    Add Method datadiri

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,4 +9,8 @@ class HomeController extends Controller
     public function home(){
         return view('welcome');
     }
+
+    public function datadiri(){
+        return "Nama : Muhammad Rizky Ramadhan <br> NIM : 1941720018 <br> Kelas : TI-2A"
+    }
 }
